0f1ebca976d5753d7a9364676293dcb025d3cec9
Chris Horikx <chris.horikx@mantelgroup.com.au>
2025-11-25 15:08:32 +1100

    test bridgeMode

--- test_container.sh
+++ test_container.sh
@@ -7,7 +7,7 @@ echo "Running container..."
 # Use a local directory for the volume to test persistence
 mkdir -p ./test_levels
 docker run -it --rm \
-  -p 3000:3000 \
+  -p 3005:3005 \
   -v $(pwd)/test_levels:/app/public/assets/levels \
   -e REPO_URL=https://github.com/horikx/td.git \
   unicorn-td:latest
